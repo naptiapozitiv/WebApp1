694504e5f8fcb08d3e947f45013f260efc792553
levanchuk2002@outlook.com <levanchuk2002@outlook.com>
2024-03-14 18:02:25 -0400

    Add project files.

---
+++ WebApp1/Controllers/FirstController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.MyBag = "Hello World";
+            return View();
+        }
+    }
+}
