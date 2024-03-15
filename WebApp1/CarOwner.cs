ea6264187b1d1eda3dda483b73f5e11c3d7d02a7
levanchuk2002@outlook.com <levanchuk2002@outlook.com>
2024-03-15 15:59:18 -0400

    HomeWork Week#22 Done

---
+++ WebApp1/CarOwner.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp1
+{
+    public class CarOwner
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Car { get; set; }
+        public string Model { get; set; }
+    }
+}
--- WebApp1/Controllers/HomeController.cs
+++ WebApp1/Controllers/HomeController.cs
@@ -15,10 +15,32 @@ namespace WebApp1.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            List<CarOwner> carOwners = new List<CarOwner>
+        {
+        new CarOwner { Name = "John", Age = 23, Car = "BMW", Model = "M5" },
+        new CarOwner { Name = "Anna", Age = 30, Car = "Audi", Model = "A4" },
+        new CarOwner { Name = "David", Age = 28, Car = "Porsche", Model = "911" },
+        new CarOwner { Name = "Emily", Age = 26, Car = "BMW", Model = "X5" },
+        new CarOwner { Name = "Mike", Age = 24, Car = "Audi", Model = "A6" },
+        new CarOwner { Name = "Jessica", Age = 22, Car = "Porsche", Model = "Cayenne" },
+        new CarOwner { Name = "Daniel", Age = 29, Car = "BMW", Model = "X1" },
+        new CarOwner { Name = "Olivia", Age = 27, Car = "Audi", Model = "RS7" },
+        new CarOwner { Name = "Sophia", Age = 21, Car = "Porsche", Model = "Carrera" },
+        new CarOwner { Name = "Stas", Age = 30, Car = "Porsche", Model = "911 GT3 RS" },
+        new CarOwner { Name = "Philip", Age = 35, Car = "Audi", Model = "Q7" }
+        };
+            return View(carOwners);
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Porsche()
+        {
+            return View();
+        }
+        public IActionResult Audi()
         {
             return View();
         }
