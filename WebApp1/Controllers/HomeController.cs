586776f3235a225b19ae58d0a2094ab1ff6211fb
levanchuk2002@outlook.com <levanchuk2002@outlook.com>
2024-03-15 16:40:08 -0400

    Added BMW Page and few details

--- WebApp1/Controllers/HomeController.cs
+++ WebApp1/Controllers/HomeController.cs
@@ -44,6 +44,10 @@ namespace WebApp1.Controllers
         {
             return View();
         }
+        public IActionResult BMW()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
